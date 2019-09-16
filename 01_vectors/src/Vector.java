1ece44c518be960d241349ec0e15149988e73871
Paolo Poso <paolo@paoloposo.de>
2019-09-16 12:30:30 +0200

    Add 01_vectors exercise

---
+++ 01_vectors/src/Vector.java
@@ -0,0 +1,163 @@
+import java.util.Objects;
+
+public class Vector {
+
+    /**
+     * The number of fixed decimal places.
+     */
+    public static final int precision = 3;
+    public static final int factor = (int) Math.pow(10, Vector.precision);
+
+    /**
+     * The zero vector.
+     */
+    public static final Vector ZERO = new Vector(0, 0, 0);
+
+    /**
+     * The x coordinate.
+     */
+    public final int x;
+
+    /**
+     * The y coordinate.
+     */
+    public final int y;
+
+    /**
+     * The z coordinate.
+     */
+    public final int z;
+
+    /**
+     * The length of the vector.
+     */
+    public final int length;
+
+    /**
+     * Create a new vector.
+     *
+     * @param x
+     * @param y
+     * @param z
+     */
+    public Vector(int x, int y, int z) {
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.length = this.calculateLength();
+    }
+
+    public int hashCode() {
+
+        return Objects.hash(this.x, this.y, this.z);
+    }
+
+    public boolean equals(Object other) {
+
+        if (other == null) return false;
+
+        else if (! (other instanceof Vector)) return false;
+
+        else {
+
+            // TODO replace "false" with the actual expression to check if the two vectors are equal
+            if (false) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    public String toString() {
+
+        return String.format("Vector{x: %d, y: %d, z: %d, length: %d}", this.x, this.y, this.z, this.length);
+    }
+
+    /**
+     * Calculate the length of the vector.
+     *
+     * @return the length of the vector
+     */
+    protected int calculateLength() {
+
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Add another vector to this vector and return the result.
+     *
+     * @param other the vector to add
+     * @return the resulting vector
+     */
+    public Vector add(Vector other) {
+
+        // TODO
+        return Vector.ZERO;
+    }
+
+    /**
+     * Subtract another vector from this vector and return the result.
+     *
+     * @param other the vector to subtract.
+     * @return the resulting vector
+     */
+    public Vector subtract(Vector other) {
+
+        // TODO
+        return Vector.ZERO;
+    }
+
+    /**
+     * Multiply the vector with a scalar.
+     *
+     * @param factor the scalar factor
+     * @return the resulting vector
+     */
+    public Vector multiply(int factor) {
+
+        // TODO
+        return Vector.ZERO;
+    }
+
+    /**
+     * Divide the vector by a scalar.
+     *
+     * @param divisor the scalar divisor
+     * @return the resulting vector
+     * @throws ArithmeticException if attempted to divide by zero
+     */
+    public Vector divide(int divisor) {
+
+        if (divisor == 0) throw new ArithmeticException("divide by zero");
+
+        // TODO
+        return Vector.ZERO;
+    }
+
+    /**
+     * Flatten the vector, so that its z component is 0, and return the result.
+     *
+     * @return the resulting, flat vector
+     */
+    public Vector flatten() {
+
+        // TODO
+        return Vector.ZERO;
+    }
+
+    /**
+     * Normalize the vector, so that its length is 1, and return the result.
+     *
+     * @return the normalized vector
+     */
+    public Vector normalize() {
+
+        // TODO
+        return Vector.ZERO;
+    }
+}
---
+++ 01_vectors/test/VectorTest.java
@@ -0,0 +1,158 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VectorTest {
+
+    public static int round(int value) {
+
+        return (int) Math.round(value / (double) Vector.factor) * Vector.factor;
+    }
+
+    @Test
+    public void testEquals1() {
+
+        Vector a = new Vector(1, 2, 3);
+        Vector b = new Vector(1, 2, 3);
+
+        assertTrue(a.equals(b));
+    }
+
+    @Test
+    public void testEquals2() {
+
+        Vector a = new Vector(1, 2, 3);
+        Vector b = new Vector(4, 5, 6);
+
+        assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void testLength1() {
+
+        Vector v = new Vector(1_000, 1_000, 0);
+        assertEquals(1_414, v.calculateLength());
+    }
+
+    @Test
+    public void testLength2() {
+
+        Vector v = new Vector(0, 0, 0);
+        assertEquals(0, v.calculateLength());
+    }
+
+    @Test
+    public void testLength3() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        assertEquals(125_000, VectorTest.round(v.length));
+    }
+
+    @Test
+    public void testAdd1() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector sum = v.add(Vector.ZERO);
+
+        assertEquals(v, sum);
+    }
+
+    @Test
+    public void testAdd2() {
+
+        Vector v1 = new Vector(35_000, 120_000, 10_000);
+        Vector v2 = new Vector(1_000, 1_000, -5_000);
+        Vector sum = v1.add(v2);
+
+        assertEquals(new Vector(36_000, 121_000, 5_000), sum);
+    }
+
+    @Test
+    public void testSubtract1() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector difference = v.subtract(Vector.ZERO);
+
+        assertEquals(v, difference);
+    }
+
+    @Test
+    public void testSubtract2() {
+
+        Vector v1 = new Vector(35_000, 120_000, 10_000);
+        Vector v2 = new Vector(1_000, 1_000, -5_000);
+        Vector difference = v1.subtract(v2);
+
+        assertEquals(new Vector(34_000, 119_000, 15_000), difference);
+    }
+
+    @Test
+    public void testMultiply1() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector product = v.multiply(0);
+
+        assertEquals(Vector.ZERO, product);
+    }
+
+    @Test
+    public void testMultiply2() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector product = v.multiply(1_000);
+
+        assertEquals(v, product);
+    }
+
+    @Test
+    public void testMultiply3() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector product = v.multiply(2_500);
+
+        assertEquals(new Vector(87_500, 300_000, 25_000), product);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivide1() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        v.divide(0);
+    }
+
+    @Test
+    public void testDivide2() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector quotient = v.divide(1_000);
+
+        assertEquals(v, quotient);
+    }
+
+    @Test
+    public void testDivide3() {
+
+        Vector v = new Vector(35_000, 120_000, 10_000);
+        Vector quotient = v.divide(2_500);
+
+        assertEquals(new Vector(14_000, 48_000, 4_000), quotient);
+    }
+
+    @Test
+    public void testFlatten() {
+
+        Vector v = new Vector(3_423, 24_356, 1_345);
+        v = v.flatten();
+        assertEquals(new Vector(3_423, 24_356, 0), v);
+    }
+
+    @Test
+    public void testNormalize() {
+
+        Vector v = new Vector(3_214, 24344_234, 326_034);
+        v = v.normalize();
+
+        int roundedLength = VectorTest.round(v.length);
+        assertEquals(1000, roundedLength);
+    }
+}
